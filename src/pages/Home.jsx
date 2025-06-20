25e80cc7062ec9ade5cda711f3608ca79e77852e
Lucrecia Parodi <parodilucrecia@gmail.com>
2025-06-20 08:52:36 +0000

    Resuelto conflicto en Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
-import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-
-export const Home = () => {
-
-  const {store, dispatch} =useGlobalReducer()
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
-}; 
-=======
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import Hero from "./hero.jsx"; 
 import About from "./About.jsx";
@@ -37,4 +19,3 @@ export const Home = () => {
     </div>
   );
 };
->>>>>>> empresa/master
